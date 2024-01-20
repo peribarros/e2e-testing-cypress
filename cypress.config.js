30b70e3749767b0a8aa6ce9bf382db2490d9f3a6
Peri Barros <peribarros@gmail.com>
2024-01-19 22:57:58 -0300

    Create e2e tests with cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
-    baseUrl: 'https://notes-serverless-app.com',
-    defaultCommandTimeout: 10000,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'https://notes-serverless-app.com'
+    env: {
+      viewportWidthBreakpoint: 768,
     },
     video: true
   },
+  projectId: "7rhrye"
 })
